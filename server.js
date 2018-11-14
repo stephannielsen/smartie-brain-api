531671fed99a3cbbc8f92152dbbfd5794aa029f8
Stephan Nielsen <stephannielsen@gmail.com>
2018-11-14 16:43:59 +0100

    Use database on default route.

--- server.js
+++ server.js
@@ -22,8 +22,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    //res.json(db.select('*').from('users'));
-    res.json("works");
+    res.json(db.select('*').from('users'));
 })
 
 app.post('/signin', signIn.handleSignIn(db, bcrypt));
